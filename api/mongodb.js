bf73a94a794ee2f9ea51c37acb8b22f2e3e9a402
Angel Serrato <serratoarias@yahoo.com>
2024-04-04 21:28:14 -0500

    Configurando vercel

--- api/mongodb.js
+++ api/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
         console.log('Connected to MongoDB');
     })
